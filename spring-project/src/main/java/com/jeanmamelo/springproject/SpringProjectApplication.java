500b8d795639067124df2b8ab25e31181c5df3b2
Jean Melo <jeanmamelo@gmail.com>
2021-06-08 20:46:16 -0300

    add crud application

--- spring-project/src/main/java/com/jeanmamelo/springproject/SpringProjectApplication.java
+++ spring-project/src/main/java/com/jeanmamelo/springproject/SpringProjectApplication.java
@@ -1,7 +1,13 @@
 package com.jeanmamelo.springproject;
 
+import com.jeanmamelo.springproject.model.Contact;
+import com.jeanmamelo.springproject.repository.ContactRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.LongStream;
 
 @SpringBootApplication
 public class SpringProjectApplication {
@@ -10,4 +16,20 @@ public class SpringProjectApplication {
 		SpringApplication.run(SpringProjectApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(ContactRepository repository) {
+		return args -> {
+			repository.deleteAll();
+			LongStream.range(1, 11)
+					.mapToObj(i -> {
+						Contact c = new Contact();
+						c.setName("Contact " + i);
+						c.setEmail("contact" + i + "@email.com");
+						c.setPhone("(111) 111-1111");
+						return c;
+					})
+					.map(v -> repository.save(v))
+					.forEach(System.out::println);
+		};
+	}
 }
---
+++ spring-project/src/main/java/com/jeanmamelo/springproject/model/Contact.java
@@ -0,0 +1,24 @@
+package com.jeanmamelo.springproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @Column(name="phone_number")
+    private String phone;
+
+}
