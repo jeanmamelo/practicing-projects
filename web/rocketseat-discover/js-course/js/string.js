fae82c4cd8600f70fc731aad8cd741ef1fae2929
Jean Melo <jeanmamelo@gmail.com>
2021-07-06 15:52:42 -0300

    add 'string' section

---
+++ web/rocketseat-discover/js-course/js/string.js
@@ -0,0 +1,14 @@
+/*
+    String
+
+    * cadeia de caracteres ABC
+
+    "" // aspas duplas
+    '' // aspas simples
+    `` // template literals ou template strings -> permite interpolação
+
+*/
+console.log("Jean")
+console.log('Jean')
+console.log(`Jean ${1+1}`)
+console.log(1+1)
