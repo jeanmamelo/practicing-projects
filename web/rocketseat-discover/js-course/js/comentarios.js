8aaa90f657c6aa3bf3c66df70426463c67c5f5b1
Jean Melo <jeanmamelo@gmail.com>
2021-07-06 13:05:00 -0300

    add 'comentarios' section

---
+++ web/rocketseat-discover/js-course/js/comentarios.js
@@ -0,0 +1,11 @@
+// aqui eu tenho um comentário em linha
+
+/*
+    aqui eu tenho um comentário em bloco
+
+    o código abaixo escreve alguma mensagem no devtools
+*/
+
+
+console.log("bem vindos ao starter") // alguma coisa console.log('vai ignorar')
+console.log("não vai ignorar")
