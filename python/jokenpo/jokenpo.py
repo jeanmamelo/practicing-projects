8b52e9a3fef257db0789f39c228b3723c0842ecb
Romulo Rueda <romulojuca@hotmail.com>
2022-10-26 21:07:48 -0300

    Update Jo-Ken-Po

--- python/jokenpo/jokenpo.py
+++ python/jokenpo/jokenpo.py
@@ -9,14 +9,14 @@ acao_jogador_1 = ""
 VITORIA = "Parabéns! Você venceu."
 DERROTA = "O PC venceu! Você perdeu seu ruim."
 
-# while acao_jogador_1 != "pedra" and acao_jogador_1 != "papel" and acao_jogador_1 != "tesoura":
-while True:
+while acao_jogador_1 != "pedra" and acao_jogador_1 != "papel" and acao_jogador_1 != "tesoura":
+# while True:
 
     acao_jogador_1 = input("Escolha a sua jogada (pedra, papel ou tesoura): ")
     print("Sua escolha:", acao_jogador_1)
 
     acao_jogador_pc = random.choice(jogadas_possiveis)
-    print("Escolha do PC:", acao_jogador_pc, "\n")
+    print("Escolha do PC:", acao_jogador_pc)
 
     if acao_jogador_1 == acao_jogador_pc:
         print("Empatou! KKKKKKKKKKKK")
@@ -34,6 +34,6 @@ while True:
         print(VITORIA)
     else:
         print("Digite apenas pedra, papel ou tesoura!\n")
-    jogar_novamente = input("Você deseja jogar novamente? (sim/nao): ")
-    if jogar_novamente != "sim":
-        break
+    # jogar_novamente = input("Você deseja jogar novamente? (sim/nao): ")
+    # if jogar_novamente != "sim":
+    #     break
