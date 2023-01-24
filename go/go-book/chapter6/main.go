6f76ddfba0b5f1c4efa860bcd3d8cfb4ace5a0b0
jeanmamelo <jeanmamelo@gmail.com>
2023-01-24 09:29:38 -0300

    Add go-book chapter six 1-4

---
+++ go/go-book/chapter6/main.go
@@ -0,0 +1,70 @@
+package main
+
+import "fmt"
+
+// 1
+func sum(numbers []int) int {
+	sum := 0
+	// for i := 0; i <= len(numbers); i++ {
+	// 	sum += i
+	// }
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+// 2
+func half(number int) (int, bool) {
+	half := number / 2
+	if number%2 == 0 {
+		return half, true
+	}
+	return half, false
+}
+
+// 3
+func greatest(numbers ...int) int {
+	greatest := 0
+	for _, number := range numbers {
+		if number > greatest {
+			greatest = number
+		}
+	}
+	return greatest
+}
+
+// 4
+func makeOddGenerator() func() uint {
+	i := uint(1)
+	return func() (ret uint) {
+		ret = i
+		i += 2
+		return
+	}
+}
+
+func main() {
+	//1
+	sum := sum([]int{1, 2, 3})
+	fmt.Println(sum)
+
+	//2
+	half1, even1 := half(1)
+	half2, even2 := half(2)
+	fmt.Println(half1, even1)
+	fmt.Println(half2, even2)
+
+	//3
+	numbers := []int{1, 13, 3, 7, 4}
+	greatest0 := greatest(numbers...)
+	fmt.Println(greatest0)
+	greatest1 := greatest(1, 13, 3, 7, 4)
+	fmt.Println(greatest1)
+
+	//4
+	nextOdd := makeOddGenerator()
+	fmt.Println(nextOdd())
+	fmt.Println(nextOdd())
+	fmt.Println(nextOdd())
+}
