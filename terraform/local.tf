40074e6805b15417dbcb8c9a0410fb4612c0be21
jeanmamelo <jeanmamelo@gmail.com>
2026-02-16 18:00:37 -0300

    feat: introduction to terraform variables

--- terraform/local.tf
+++ terraform/local.tf
@@ -1,4 +1,6 @@
 resource "local_file" "example" {
     filename = "example.txt"
-    content = "This is an example of a local file created and updated by Terraform."
+    content = var.content
 }
+
+variable "content" {}
